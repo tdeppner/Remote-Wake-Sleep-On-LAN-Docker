b453d710f0f4cfe24433399a5ef6aea3e6203c86
Ted Deppner <ted@deppner.us>
2024-07-22 16:34:19 -0700

    add container health checking, and a /health endpoint

---
+++ build/ping.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'config.php';
+
+// Do this separately to avoid a php failure outputting the raw php file and then
+// the phrase being present for a simple grep to find.
+echo "O";
+echo "K";
+echo "\n";
